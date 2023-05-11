5153739066fe8d9ea93dd69d07442e9121f3cf7d
DavidH <handuy099@gmail.com>
2023-05-11 21:20:40 +0700

    UI[Fix]: Fix image and format code

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,18 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-import classNames from 'classnames/bind'
-import styles from './AccountItem.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img
-        className={cx('avatar')}
-        src='https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/68539fa0a7334eba095bcd494efa88be~c5_100x100.jpeg?x-expires=1663916400&x-signature=WmGjqeuyZTCnSRFeFqmm50c%2FGKw%3D'
-        alt='Hoaa'
-      />
+      <img className={cx('avatar')} src="https://source.unsplash.com/user/c_v_r/100x100" alt="Hoaa" />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
           <span>Hoaa989</span>
@@ -21,7 +17,7 @@ function AccountItem() {
         <span className={cx('username')}>hoaa</span>
       </div>
     </div>
-  )
+  );
 }
 
-export default AccountItem
+export default AccountItem;
