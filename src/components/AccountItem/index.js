d1a79ca86dfdefa600fe145b7c6d2327cfb805f0
DavidH <handuy099@gmail.com>
2023-05-21 18:37:30 +0700

    UI[Header/Add]: Handle error load image

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '~/components/image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img className={cx('avatar')} src="https://source.unsplash.com/user/c_v_r/100x100" alt="Hoaa" />
+      <Image className={cx('avatar')} src="https://source.unsplash.com/user/c_v_r/100x100" alt="Hoaa" />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
           <span>Hoaa989</span>
