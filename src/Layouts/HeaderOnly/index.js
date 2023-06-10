fc0f90cfa106259a159197cf89b3e850d5ec5023
DavidH <handuy099@gmail.com>
2023-06-10 23:26:54 +0700

    UI[Container]: Fix not use default layout css

--- src/Layouts/HeaderOnly/index.js
+++ src/Layouts/HeaderOnly/index.js
@@ -1,11 +1,16 @@
+import classNames from 'classnames/bind';
+
 import Header from '../components/Header';
+import styles from '../DefaultLayout/DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function DefaultLayout({ children }) {
   return (
     <div>
       <Header />
-      <div className="container">
-        <div className="content">{children}</div>
+      <div className={cx('container')}>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
